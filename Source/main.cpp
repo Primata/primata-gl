8a0998d2c75ba982273304cc603f942ac39acfdc
Carlos Sousa <karl_spt@yahoo.com>
2014-11-25 15:30:02 +0000

    Primeira versao do jogo. Consiste simplesmente num programa que escreve "Hello World" no terminal.

---
+++ Source/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Klephtica
+//
+//  Created by Carlos Sousa on 2014-11-25.
+//  Copyright (c) 2014 primatas. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char ** argv)
+{
+    
+    cout << "Hello World!" << endl;
+    return 0;
+}
